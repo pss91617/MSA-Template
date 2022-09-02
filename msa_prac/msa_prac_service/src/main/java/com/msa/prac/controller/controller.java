97f6a7f77518401ea35d71cd51257344e55ca32b
류승재(누리플렉스) <pss91617@nuriflex.co.kr>
2022-09-02 17:07:21 +0900

    MSA-Template ( MAVEN )

---
+++ msa_prac/msa_prac_service/src/main/java/com/msa/prac/controller/controller.java
@@ -0,0 +1,22 @@
+package com.msa.prac.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class controller {
+
+    @PostMapping("/sssssss")
+    public void testxx(){
+
+        System.out.println("tttttt");
+
+    }
+
+}
+//public class controller{
+//
+//    public static void main(String[] args) {
+//        System.out.println("Hello World!");
+//    }
+//}
